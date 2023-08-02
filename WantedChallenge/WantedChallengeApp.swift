b8875c23710acecb3ae8e376f4970a37bbc0b594
Choi Hyunjin <guswls3373@gmail.com>
2023-08-02 19:45:14 +0900

    feat: imp chapter 1 (#1)

---
+++ WantedChallenge/WantedChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WantedChallengeApp.swift
+//  WantedChallenge
+//
+//  Created by Tophy on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct WantedChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
